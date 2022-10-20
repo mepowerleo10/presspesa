19e359e8121732f07cbfcbff7bf4a42558282f54
Mussa Mipawa <mepowerleo10@gmail.com>
2022-10-20 06:33:51 +0300

    fix: modify authentication to use a service path - add .env to gitignore - use the env from configurations module to query environment variables - change the Certificate parameters to read a configration file path instead of a dictionary

--- server/firebase_auth/authentication.py
+++ server/firebase_auth/authentication.py
@@ -9,20 +9,9 @@ from django.contrib.auth import get_user_model
 
 from firebase_auth.exceptions import NoAuthToken, InvalidAuthToken, FirebaseError
 
-cred = credentials.Certificate(
-    {
-        "type": "service_account",
-        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-        "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
-    }
-)
+from config.settings import env
+
+cred = credentials.Certificate(env("FIREBASE_SERVICE_ACCOUNT_KEY_PATH"))
 
 default_app = firebase_admin.initialize_app(cred)
 
@@ -38,7 +27,7 @@ class FirebaseAuthentication(authentication.BaseAuthentication):
             decoded_token = auth.verify_id_token(id_token)
             uid = decoded_token.get("user_id")
 
-            print (decoded_token.get("user_id"))
+            print(decoded_token.get("user_id"))
         except Exception:
             raise InvalidAuthToken("Invalid auth token")
 
